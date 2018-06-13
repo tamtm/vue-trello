cd5f00925afb0cc36b69e555e27f4751fee8f17e
tamtm <tamtm.dev@gmail.com>
2018-06-13 16:05:40 +0700

    init

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <h1>{{ msg }}</h1>
+    <p>Powered by <a href="https://github.com/vuejs/vue" target="_blank">Vuejs</a></p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'vue-trello'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+a {
+  color: #42b983;
+}
+</style>
